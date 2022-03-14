a34855cf0376b666cef72d06ee80b48abc43a5a3
vadith gomez <legrone@MacBook-Pro.local>
2022-03-13 19:09:47 -0500

    Authentication-errors

--- src/components/principal/LoginScreen/LoginScreen.jsx
+++ src/components/principal/LoginScreen/LoginScreen.jsx
@@ -6,36 +6,54 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import "./LoginScreen.css"
 
+function validate(credentials) {
+    let errors = {};
+    if (!credentials.email) {
+        errors.email = 'please enter your email address correctly'
+    }
+    if (!credentials.password || credentials.password < 0) {
+        errors.password = 'please enter your password correctly'
+    }
+    return errors
+}
 
 function LoginScreen() {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-
+    const [errors, setErrors] = useState({})
     const [credentials, setCredentials] = useState({
         email: '',
         password: ''
     })
-
+console.log(errors)
     const user = useSelector(state => state.user)
 
-    useEffect(() => {
+    /* useEffect(() => {
         if (user.firstName) {
             navigate('/home')
         }
         // eslint-disable-next-line
-    }, [user])
+    }, [user]) */
 
     function handleInputChange(e) {
         setCredentials({
             ...credentials,
             [e.target.name]: e.target.value
         })
+        setErrors(validate({
+            ...credentials,
+            [e.target.name]: e.target.value
+        }));
     }
 
     const signIn = (e) => {
         e.preventDefault()
-        dispatch(login(credentials))
+        if (Object.values(errors).length > 0 || !errors.email) alert("Por favor rellenar todos los campos")
+        else {
+            dispatch(login(credentials))
+            navigate('/home')
+        }
     }
 
     const register = () => {
@@ -46,8 +64,19 @@ function LoginScreen() {
         <div className="signup_m">
             <form onSubmit={e => signIn(e)}>
                 <h1>Sign In</h1>
-                <input placeholder='Email' type="email" value={credentials.email} name='email' onChange={e => handleInputChange(e)} />
-                <input placeholder='Password' type="password" value={credentials.password} name='password' onChange={e => handleInputChange(e)} />
+                <div>
+                    <input placeholder='Email' type="email" value={credentials.email} name='email' onChange={e => handleInputChange(e)} />
+                {errors.email && (
+                    <p>{errors.email}</p>
+                )}
+                </div>
+                <div>
+                    <input placeholder='Password' type="password" value={credentials.password} name='password' onChange={e => handleInputChange(e)} />
+                {errors.password &&  (
+                    <p>{errors.password}</p>
+                )}
+                </div>
+                
                 <button type='submit' >Sign In</button>
                 <h4>
                     <span className='signup_gray' >New to Netflix?  </span>
